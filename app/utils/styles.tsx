c381217371a69a2ed4d787144d8bdb8d506b5e9f
Yaşar Kaan Vural <110562631+kaan1iq0@users.noreply.github.com>
2025-09-15 23:59:58 +0300

    gorunum

--- app/utils/styles.tsx
+++ app/utils/styles.tsx
@@ -1,5 +1,47 @@
 import { StyleSheet } from "react-native"
+import { fontSizes } from "./settings"
+import { Surface } from "react-native-paper"
 
-export const styles = StyleSheet.create({
+export const stylesSettings = StyleSheet.create({
+    setting:{
+        
 
+     
+    } ,
+    surface: {
+        margin: 'auto',
+        borderRadius: 6,
+        paddingRight: 10,
+        paddingLeft: 10,
+        paddingBottom: 15,
+        paddingTop: 10,
+        height: 'auto',
+        width: '80%',
+        alignItems: 'center',        
+        justifyContent: 'center',
+    },
+    divider:{
+        marginTop:5,
+        marginBottom:5,
+        width: '100%'
+    },
+    rowbox:{
+        flexDirection: 'row',
+        alignItems:'center',
+        justifyContent: 'space-around',
+    
+        width: '100%',
+   
+    },
+    columnbox:{
+        flexDirection: 'column',
+        alignItems:'center',
+        justifyContent: 'center',
+      
+    },
+     selected:{
+        borderWidth: 2,
+        borderColor: '#388E3C',
+      
+    }
 })
--- app/views/pages/ayarlar/GenelAyarlar.tsx
+++ app/views/pages/ayarlar/GenelAyarlar.tsx
@@ -1,11 +1,43 @@
-import { View } from 'react-native'
+import { Settings, View } from 'react-native'
 import React from 'react'
-import { Text } from 'react-native-paper'
+import { Text , Surface, IconButton, MD3Colors,Divider, DividerProps} from 'react-native-paper'
+
+import { stylesSettings } from '../../../utils/styles'
+
 
 const GenelAyarlar = () => {
   return (
-    <View>
-      <Text>Genel Ayarlar</Text>
+    <View style={stylesSettings.setting}>
+      <Surface style={stylesSettings.surface} elevation={5}>
+          <Text  variant="titleMedium">Görünüm</Text>
+         <Divider style={stylesSettings.divider} />
+          <View style={stylesSettings.rowbox}>
+             <View style={stylesSettings.columnbox}>
+                <IconButton style={stylesSettings.selected}
+                  mode='contained'
+                  icon=  "tooltip-image-outline"
+                  iconColor={'#388E3C'}
+                  size={50}
+                  onPress={() => console.log('Pressed')}
+                />
+                <Text  variant="titleSmall">Resim</Text>
+             </View>
+           
+            <View style={stylesSettings.columnbox}>
+              <IconButton
+                mode='contained'
+                icon= "tooltip-text-outline"
+                size={50}
+               
+                onPress={() => console.log('Pressed')}
+              />
+              <Text  variant="titleSmall">Yazı</Text>
+            </View>
+  
+          </View>
+
+
+      </Surface>
     </View>
   )
 }
